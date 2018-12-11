44cf0ad06d5a2f9a09c7ccee53bb7d2aded97662
mansiboriya <mansiboriya@gmail.com>
2018-12-10 19:27:18 -0500

    Blazor-App-Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using System;
+
+namespace BlazorBricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(configure =>
+            {
+                // Add any custom services here
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+    }
+
+    public static class OnKeyUp
+    {
+        public static Action<string> Action { get; set; }
+        public static void Handler(string value)
+        {
+            Action?.Invoke(value);
+        }
+    }
+}
